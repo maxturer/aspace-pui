80dbcbb43b20477d1ebbdb4e5d76955ecd6da0a6
Max Turer <max.turer@gwu.edu>
2024-07-11 17:25:10 -0400

    added all CSS and updated ERB templates, fixed facet bug

--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -1,5 +1,7 @@
-AppConfig[:pui_branding_img] = '/assets/images/gw_iddol_lib_2cs_rev-01.png'
+AppConfig[:pui_branding_img] = '/assets/images/logo.png'
 AppConfig[:pui_branding_img_alt_text] = 'GW Libraries & Academic Innovation'
+AppConfig[:pui_footer_img] = '/assets/images/gw_iddol_libraries_2c-320.png'
+AppConfig[:pui_main_img] = '/assets/images/gw_primary_2c_rev.png'
 
 ## The following determine which 'tabs' are on the main horizontal menu
 #AppConfig[:pui_hide] = {}
